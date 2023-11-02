01b03e7cfad1389fc7da1cd870fa2e1c43e8f064
ThexdxrBlck <morobe76o@gmail.com>
2023-11-02 14:57:03 -0700

    more even

--- 11-binary_tree_size.c
+++ 11-binary_tree_size.c
@@ -5,7 +5,7 @@
  * @tree: A pointer to the root node of the tree to measure the size of
  * Return: The size of the tree
  */
-size_t binary_tree_size(const binaary_tree_t *tree)
+size_t binary_tree_size(const binary_tree_t *tree)
 {
 	size_t size = 0;
 
